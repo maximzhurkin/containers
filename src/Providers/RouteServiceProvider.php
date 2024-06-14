655395a26a79b985841a5b9bc71ba8b902b944d3
Max Zhurkin <maximzhurkin@gmail.com>
2024-06-14 17:00:41 +0300

    return exception

--- src/Providers/RouteServiceProvider.php
+++ src/Providers/RouteServiceProvider.php
@@ -40,12 +40,12 @@ class RouteServiceProvider extends ServiceProvider
                 ! class_exists($registrar) ||
                 ! is_subclass_of($registrar, RouteRegistrar::class)
             ) {
-//                throw new RuntimeException(
-//                    sprintf(
-//                        'Cannot map routes \'%s\', it is not a valid routes class',
-//                        $registrar
-//                    )
-//                );
+                throw new RuntimeException(
+                    sprintf(
+                        'Cannot map routes \'%s\', it is not a valid routes class',
+                        $registrar
+                    )
+                );
             }
 
             (new $registrar())->map($router);
